9fe1b7fbd94f6e43945634b473c921a781b6ee04
484021 <bhoopalsanthosh@gmail.com>
2024-08-28 19:26:29 -0400

    feat: fixed marketing page's styling

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -5,22 +5,22 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
+    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10 ">
       <Image
         src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
         alt="Preview of Petly"
         width={519}
         height={472}
       />
-      <div>
+      <div className="flex flex-col text-center items-center">
         <Logo />
         <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
-          Manage your <span className="font-extrabold">pet daycare</span> with
+          Manage your <span className="font-extrabold ">pet daycare</span> with
           ease
         </h1>
         <p className="text-2xl font-medium max-w-[600px]">
           Use Petly to easily keep track of pets under your care. Get lifetime
-          access for $299
+          access for $199
         </p>
         <div className="mt-10 space-x-3">
           <Button asChild><Link href="/signup">Get started</Link></Button>
