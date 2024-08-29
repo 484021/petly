e1d78ac78cf294c942a8fed2ec4b1e4092f5ca21
484021 <bhoopalsanthosh@gmail.com>
2024-08-28 20:32:57 -0400

    feat: added test credentials for login and signup

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,8 +6,13 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main>
+      <div className="text-center">
+        <H1 className="mb-2">Test Credentials</H1>
+      </div>
+      <p>User: example@gmail.com</p>
+      <p>Password: example</p>
       <H1 className="text-center mb-5">Log In</H1>
-      <AuthForm type="logIn"/>
+      <AuthForm type="logIn" />
       <p className="mt-6 text-sm text-zinc-500">
         No Account yet?{" "}
         <Link href="/signup" className="font-medium">
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -6,6 +6,10 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main>
+      <div className="text-center">
+        <H1 className="mb-2">Stripe Test Credit Card</H1>
+      </div>
+      <p>Card Number: 4242 4242 4242 4242</p>
       <H1 className="text-center mb-5">Sign Up</H1>
       <AuthForm type="signUp"/>
       <p className="mt-6 text-sm text-zinc-500">
