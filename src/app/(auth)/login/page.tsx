4cca7bb35f205b7436b4e49fb8cd6974322e679f
484021 <bhoopalsanthosh@gmail.com>
2024-08-30 08:09:21 -0400

    feat: made test credentials more asthetic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,23 +2,42 @@ import AuthForm from "@/components/auth-form";
 import H1 from "@/components/h1";
 import Logo from "@/components/logo";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
-    <main>
-      <div className="text-center">
-        <H1 className="mb-2">Test Credentials</H1>
-      </div>
-      <p>User: example@gmail.com</p>
-      <p>Password: example</p>
-      <H1 className="text-center mb-5">Log In</H1>
+    <main className=" flex flex-col items-center">
+    
+      
+      <H1 className="text-center mb-5 mt-5">Log In</H1>
       <AuthForm type="logIn" />
-      <p className="mt-6 text-sm text-zinc-500">
+      <p className="mt-6 text-sm text-zinc-500 mb-5">
         No Account yet?{" "}
         <Link href="/signup" className="font-medium">
           Sign Up
         </Link>
       </p>
+      <Dialog >
+        <DialogTrigger>
+          <Button>Test Credentials</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Test Credentials</DialogTitle>
+            <DialogDescription>User: example@gmail.com</DialogDescription>
+            <DialogDescription>Password: example</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
     </main>
   );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,22 +2,43 @@ import AuthForm from "@/components/auth-form";
 import H1 from "@/components/h1";
 import Logo from "@/components/logo";
 import Link from "next/link";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+
 
 export default function Page() {
   return (
-    <main>
-      <div className="text-center">
-        <H1 className="mb-2">Stripe Test Credit Card</H1>
-      </div>
-      <p>Card Number: 4242 4242 4242 4242</p>
+    <main className="flex flex-col">
+      
       <H1 className="text-center mb-5">Sign Up</H1>
       <AuthForm type="signUp"/>
-      <p className="mt-6 text-sm text-zinc-500">
+      <p className="mt-6 text-sm text-zinc-500 mb-5">
         Already have an account?{" "}
         <Link href="/login" className="font-medium">
           Log In
         </Link>
       </p>
+      <Dialog >
+        <DialogTrigger>
+          <Button>Test Credit Card</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Test Credit Card</DialogTitle>
+            <DialogDescription>CC#: 4242 4242 4242 4242</DialogDescription>
+            <DialogDescription>CVC: Any</DialogDescription>
+
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
     </main>
   );
 }
